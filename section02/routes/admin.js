6f2002b1705d0a6e8470deca2a9bf64ac9138808
MICHAEL DE SPAIN <mdespain35@gmail.com>
2021-10-01 12:54:16 -0700

    Initial catchup commit

--- section02/routes/admin.js
+++ section02/routes/admin.js
@@ -2,28 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsCon = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get(productsCon.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title, price: req.body.price, description: req.body.description});
-  res.redirect('/');
-});
+router.post(productsCon.postAddProduct);
 
 exports.routes = router;
-exports.products = products;
--- section02/routes/shop.js
+++ section02/routes/shop.js
@@ -2,21 +2,11 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsCon = require('../controllers/products')
 const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsCon.getProducts);
 
 module.exports = router;
